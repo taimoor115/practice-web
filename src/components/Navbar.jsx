42ddabc04225f7a860e502e34ea91c32caec3ea0
Taimoor Hussain <taimoorhussain985@gmail.com>
2024-06-04 00:54:28 +0500

    Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="flex px-2 py-4 justify-between w-full bg-slate-500">
+      <span className="font-bold text-xl lg:text-2xl">Webify Shopping</span>
+      <div className="w-32  font-bold text-xl hidden lg:block space-x-4">
+        <Link to={"/"}>Home</Link>
+        <Link to={"/cart"}>Cart</Link>
+      </div>
+      <Link to={"/cart"} className="flex space-x-3 text-center text-lg">
+        <img
+          src="/src/assets/cart.svg"
+          alt="cart image"
+          height={30}
+          width={30}
+        />
+        <p>Items: 0</p>
+      </Link>
+    </div>
+  );
 };
 
 export default Navbar;
--- src/route.jsx
+++ src/route.jsx
@@ -1,11 +1,15 @@
 import { createBrowserRouter } from "react-router-dom";
 import Layout from "./pages/Layout";
+import Cart from "./pages/Cart";
 import Home from "./pages/Home";
 const route = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
-    children: [{ path: "/", element: <Home /> }],
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/cart", element: <Cart /> },
+    ],
   },
 ]);
 export default route;
